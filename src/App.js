bef289af936398cf0772d9585ae24b6691e0bdce
Ted J <tedmjablonowski@gmail.com>
2022-08-03 07:41:43 -0400

    Initial React files for front end testing purposes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Homepage'
+import './App.css';
+
+function App() {
+  return (
+    <Home></Home>
+  );
+}
+
+export default App;
